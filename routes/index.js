9865d457c51703e8328f095713391aac706c978d
Savvas Petridis <sdp2137@columbia.edu>
2015-02-27 14:23:44 -0500

    good format/routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
-// written by Savvas Petridis
+var express = require('express');
+var router = express.Router();
+// could use one line instead: var router = require('express').Router();
+var tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+});
+
+module.exports = router;
