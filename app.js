ce1e11963b9ba7bcaec0376e576998f65a101e25
Savvas Petridis <sdp2137@columbia.edu>
2015-02-27 13:04:18 -0500

    server set-up

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // written by Savvas Petridis
+
+var express = require( 'express' );
+var morgan = require( 'morgan' );
+var app = express();
+
+app.use(morgan('dev'))
+
+var server = app.listen(3000, function () 
+{
+	console.log('server listening')
+})
+
+app.get('/', function (req, res) 
+{
+  res.send('Welcome to the server!!')
+})
